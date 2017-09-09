4bbb844981980c260e147bcbdcc4c5b97b1df0c3
Chatchai Saratakij <CSaratakij@gmail.com>
2017-09-09 18:34:44 +0700

    Make picture dialog to show dialog with image in it.

---
+++ Labsheet01_A/Labsheet01_A/Forms/PictureDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Labsheet01_A.Forms
+{
+    public partial class PictureDialog : Form
+    {
+        public PictureDialog()
+        {
+            InitializeComponent();
+        }
+
+        public PictureDialog(string title, string message, Bitmap image) : this()
+        {
+            this.Text = title;
+            rchMessage.Text = message;
+            pictureBox.Image = image;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
